bcea741a8943a4ca80cde5df8ad299d6e697390d
swiftzhang125 <55674550+swiftzhang125@users.noreply.github.com>
2025-01-12 14:47:16 +0800

    Update cembed.py

--- criteria_embedding/cembed.py
+++ criteria_embedding/cembed.py
@@ -1,12 +1,10 @@
 import os
 import torch
 
-import sys
-sys.path.insert(0, '/home/yzhang37/Trials/')
 from utils.embedding import Embedding as EMB
 from utils.utils_whole import save_pkl
 
-DATA_PATH = os.environ.get('DATA_PATH', '/home/yzhang37/data/clinicalTrialsPrediction/clinical-trial-outcome-prediction/data/raw_data.csv')
+DATA_PATH = os.environ.get('DATA_PATH', './data/clinicalTrialsPrediction/clinical-trial-outcome-prediction/data/raw_data.csv')
 
 
 class criteriaEmbedding(EMB):
